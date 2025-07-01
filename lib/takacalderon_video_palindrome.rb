c738cbd4014cd5d8ae32b66fc7bc083767b95473
Taka Calderon <taka.calderon@gmail.com>
2025-07-01 09:25:24 -0700

    Initialize repository

---
+++ lib/takacalderon_video_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "takacalderon_video_palindrome/version"
+
+module TakacalderonVideoPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
