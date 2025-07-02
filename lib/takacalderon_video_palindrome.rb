5dc027f410dd9ffd86a6995ad93fdc2ab178ea3f
Taka Calderon <taka.calderon@gmail.com>
2025-07-02 11:56:17 -0700

    Factor into module, add integer palindrome detection

--- lib/takacalderon_video_palindrome.rb
+++ lib/takacalderon_video_palindrome.rb
@@ -1,19 +1,23 @@
 require_relative "takacalderon_video_palindrome/version"
 
 module TakacalderonVideoPalindrome
+    # Returns true for a palindrome, false otherwise.
+    def palindrome?
+      processed_content == processed_content.reverse
+    end
+  
+    private
+  
+      # Returns content for palindrome testing.
+      def processed_content
+        self.to_s.scan(/[a-z\d]/i).join.downcase
+      end
 end
 
 class String
+  include TakacalderonVideoPalindrome
+end
 
-  # Returns true for a palindrome, false otherwise.
-  def palindrome?
-    processed_content == processed_content.reverse
-  end
-
-  private
-
-    # Returns content for palindrome testing.
-    def processed_content
-      self.scan(/[a-z]/i).join.downcase
-    end
+class Integer
+  include TakacalderonVideoPalindrome
 end
